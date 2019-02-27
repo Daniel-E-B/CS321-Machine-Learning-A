53173496ea846fcd1cc3619f2849f7d59fc96200
Daniel Bernstein <danbern@nuevaschool.org>
2019-02-27 13:48:17 -0800

    start working on setting output

---
+++ gan/GANtime.py
@@ -0,0 +1,251 @@
+import numpy as np
+import os
+import sys
+import matplotlib.pyplot as plt
+from keras.datasets import mnist
+from keras.layers import Input, Dense, Reshape, Flatten, Dropout
+from keras.layers import BatchNormalization, Activation, ZeroPadding2D
+from keras.layers.advanced_activations import LeakyReLU
+from keras.layers.convolutional import UpSampling2D, Conv2D
+from keras.models import Sequential, Model
+from keras.optimizers import Adam
+from keras.utils import to_categorical
+
+import matplotlib
+matplotlib.use('Agg')  # no x server
+import matplotlib.pyplot as plt
+
+
+images_dir = "dcgan_images_m"
+img_rows = 28
+img_cols = 28
+channels = 1
+noise_len = 100
+num_digits = 10
+
+
+def build_discriminator():
+    '''
+    Put together a CNN that will return a single confidence output.
+
+    returns: the model object
+    '''
+
+    img_shape = (img_rows, img_cols, channels)
+
+    model = Sequential()
+
+    model.add(Conv2D(32, kernel_size=3, strides=2,
+                     input_shape=img_shape, padding="same"))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(Dropout(0.25))
+    model.add(Conv2D(64, kernel_size=3, strides=2, padding="same"))
+    model.add(ZeroPadding2D(padding=((0, 1), (0, 1))))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(Dropout(0.25))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(Conv2D(128, kernel_size=3, strides=2, padding="same"))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(Dropout(0.25))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(Conv2D(256, kernel_size=3, strides=1, padding="same"))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(1, activation='sigmoid'))
+
+    return model
+
+
+def build_generator():
+    '''
+    Put together a model that takes in one-dimensional noise and outputs two-dimensional
+    data representing a black and white image, with -1 for black and 1 for white.
+
+    returns: the model object
+    '''
+
+    noise_shape = (noise_len + num_digits,)
+
+    model = Sequential()
+
+    model.add(Dense(128 * 7 * 7, activation="relu", input_shape=noise_shape))
+    model.add(Reshape((7, 7, 128)))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(UpSampling2D())
+    model.add(Conv2D(128, kernel_size=3, padding="same"))
+    model.add(Activation("relu"))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(UpSampling2D())
+    model.add(Conv2D(64, kernel_size=3, padding="same"))
+    model.add(Activation("relu"))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(Conv2D(1, kernel_size=3, padding="same"))
+    model.add(Activation("tanh"))
+
+    return model
+
+
+def build_combined():
+    '''
+    Puts together a model that combines the discriminator and generator models.
+
+    returns: the generator, discriminator, and combined model objects
+    '''
+
+    optimizer = Adam(0.0002, 0.5)
+
+    # Build and compile the discriminator
+    discriminator = build_discriminator()
+    discriminator.compile(loss='binary_crossentropy',
+                          optimizer=optimizer,
+                          metrics=['accuracy'])
+
+    # Build and compile the generator
+    generator = build_generator()
+    generator.compile(loss='binary_crossentropy', optimizer=optimizer)
+
+    # The generator takes noise (and the onehot encoded digit) as input and generates images
+    noise = Input(shape=(noise_len + num_digits,))
+    img = generator(noise)
+
+    # For the combined model we will only train the generator
+    discriminator.trainable = False
+
+    # The discriminator takes generated images as input and determines validity
+    valid = discriminator(img)
+
+    # The combined model  (stacked generator and discriminator) takes
+    # noise as input => generates images => determines validity
+    combined = Model(inputs=noise, outputs=valid)
+    combined.compile(loss='binary_crossentropy', optimizer=optimizer)
+    return generator, discriminator, combined
+
+
+def save_imgs(generator, epoch):
+    '''
+    Has the generator create images and saves the images in a single file that includes
+    the epoch in the filename.
+
+    inputs:
+        generator: the generator model object returned by build_combined
+        epoch: the epoch number (but can be anything that can be represented as a string)
+
+    returns: None
+    '''
+    r, c = 5, 5
+    noise = np.random.normal(0, 1, (r * c, noise_len))
+    # only generate a certain thing
+    # num = np.eye(num_digits)[np.random.choice(num_digits, r * c)]
+    num = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 1]] * 5 * 5) # only generate 9's
+
+    input = np.concatenate((noise, num), axis=1)
+    gen_imgs = generator.predict(input)
+
+    # Rescale images 0 - 1
+    gen_imgs = 0.5 * gen_imgs + 0.5
+
+    fig, axs = plt.subplots(r, c)
+    #fig.suptitle("DCGAN: Generated digits", fontsize=12)
+    cnt = 0
+    for i in range(r):
+        for j in range(c):
+            axs[i, j].imshow(gen_imgs[cnt, :, :, 0], cmap='gray')
+            axs[i, j].axis('off')
+            cnt += 1
+    fig.savefig(os.path.join(images_dir, 'mnist_{}.png'.format(epoch)))
+    plt.close()
+
+
+def train(generator, discriminator, combined, epochs, batch_size=128, save_interval=50):
+    '''
+    Trains all model objects
+
+    generator: the generator model object returned by build_combined
+    discriminator: the discriminator model object returned by build_combined
+    combined: the combined model object returned by build_combined
+    epochs: integer, the number of epochs to train for
+    batch_size: integer, the number of training samples to use at a time
+    save_interval: integer, will generate and save images when the current epoch % save_interval is 0
+
+    returns: None
+    '''
+
+    # Load the dataset
+    (X_train, Y_train), (_, _) = mnist.load_data()
+
+    # Rescale -1 to 1
+    X_train = (X_train.astype(np.float32) - 127.5) / 127.5
+    X_train = np.expand_dims(X_train, axis=3)
+    Y_train = to_categorical(Y_train)
+
+    half_batch = int(batch_size / 2)
+
+    for epoch in range(epochs):
+
+        # ---------------------
+        #  Train Discriminator
+        # ---------------------
+
+        # Select a random half batch of images
+        idx = np.random.randint(0, X_train.shape[0], half_batch)
+        imgs = X_train[idx]
+        num = Y_train[idx]
+
+        # Sample noise and generate a half batch of new images
+        noise = np.random.normal(0, 1, (half_batch, noise_len))
+        # num = np.eye(num_digits)[np.random.choice(num_digits, half_batch)]
+        input = np.concatenate((noise, num), axis=1)
+        gen_imgs = generator.predict(input)
+
+        # Train the discriminator (real classified as ones and generated as zeros)
+        # TODO: discriminator has 11 outputs. Real/fake, 0/not a 0, 1/not a 1, 2/not a 2, etc
+        d_loss_real = discriminator.train_on_batch(
+            imgs, np.ones((half_batch, 1)))
+        d_loss_fake = discriminator.train_on_batch(
+            gen_imgs, np.zeros((half_batch, 1)))
+
+        # ---------------------
+        #  Train Generator
+        # ---------------------
+
+        idx = np.random.randint(0, X_train.shape[0], batch_size)
+        imgs = X_train[idx]
+        num = Y_train[idx]
+        noise = np.random.normal(0, 1, (batch_size, noise_len))
+        input = np.concatenate((noise, num), axis=1)
+
+        # Train the generator (wants discriminator to mistake images as real)
+        g_loss = combined.train_on_batch(input, np.ones((batch_size, 1)))
+
+        # If at save interval => save generated image samples and plot progress
+        if epoch % save_interval == 0:
+            # Plot the progress
+            d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+            print("{} [D loss: {}, acc.: {:.2%}] [G loss: {}]".format(
+                epoch, d_loss[0], d_loss[1], g_loss))
+            save_imgs(generator, epoch)
+
+
+def show_new_image(generator):
+    '''
+    Generates and displays a new image
+
+    inputs: generator object model returned from build_combined
+
+    returns: generated image
+    '''
+
+    noise = np.random.normal(0, 1, (1, noise_len))
+    gen_img = generator.predict(noise)[0][:, :, 0]
+
+    return plt.imshow(gen_img, cmap='gray', vmin=-1, vmax=1)
+
+
+if (not os.path.isdir(images_dir)):
+    os.mkdir(images_dir)
+
+generator, discriminator, combined = build_combined()
+train(generator, discriminator, combined,
+      epochs=4001, batch_size=32, save_interval=50)
