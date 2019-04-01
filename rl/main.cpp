dfaee60ef65b058f8371becb3bbd229769c6fc7a
Daniel Bernstein <danbern@nuevaschool.org>
2019-04-01 14:32:29 -0700

    nn proof of concept

--- rl/main.cpp
+++ rl/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 #include <SFML/Graphics.hpp>
 
@@ -6,6 +7,24 @@
 #include "tiny-dnn/tiny_dnn/tiny_dnn.h"
 
 int main() {
+    tiny_dnn::network<tiny_dnn::sequential> net;
+    tiny_dnn::adagrad opt;
+    net << tiny_dnn::layers::fc(2, 3) << tiny_dnn::activation::leaky_relu()
+        << tiny_dnn::layers::fc(3, 1) << tiny_dnn::activation::sigmoid();
+
+    std::vector<tiny_dnn::vec_t> x_train{{0, 0}, {0, 1}, {1, 0}, {1, 1}};
+    std::vector<tiny_dnn::vec_t> y_train{{0}, {1}, {1}, {0}};
+    size_t batch_size = 2;
+    size_t epochs = 300;
+
+    net.fit<tiny_dnn::mse>(opt, x_train, y_train, batch_size, epochs);
+    tiny_dnn::vec_t result = net.predict({1, 0});
+    std::cout << result[0] << std::endl;
+    result = net.predict({1, 1});
+    std::cout << result[0] << std::endl;
+    net.save("net", tiny_dnn::content_type::weights, tiny_dnn::file_format::json);
+    // net.load("net", tiny_dnn::content_type::weights, tiny_dnn::file_format::);
+
     const int WIDTH = 1280, HEIGHT = 720;
 
     sf::ContextSettings ctx;
