3d77c1197ba5aebff8c22aa81e04890e41710db0
Daniel Bernstein <danbern@nuevaschool.org>
2019-05-17 15:04:53 -0700

    write basic scraper

--- final/scraper.js
+++ final/scraper.js
@@ -2,14 +2,19 @@ var scraper = require('google-play-scraper');
 var d3 = require('d3-dsv');
 var fs = require('fs');
 
-var txt = fs.readFileSync('apps.csv', 'utf8').toString();
+var txt = fs.readFileSync('../apps.csv', 'utf8').toString();
+// apps.csv from https://www.kaggle.com/orgesleka/android-apps#apps.csv
 
 var data = d3.csvParse(txt);
 
-ids = [];
+// data.length == 403910
 
-for (let i = 0; i < data.length; ++i) {
-    ids.push(data[i].packageName);
+for (let i = 150500; i < 161000; ++i) {
+    try {
+        scraper.app({ appId: data[i].packageName }).then((app) => {
+            fs.writeFile('app' + i + '.json', JSON.stringify(app), (err) => {
+                if (err) throw err
+            });
+        }).catch((e) => { });
+    } catch (e) { }
 }
-
-console.log(ids);
