197b7f3950b03ed6315355d980091a1459bd9116
Daniel Bernstein <danbern@nuevaschool.org>
2019-05-29 15:45:02 -0700

    work on the ascii-fying

--- final/json2csv.py
+++ final/json2csv.py
@@ -2,7 +2,8 @@ import csv
 import json
 import os
 
-important_things = ["title", "description", "summary", "score", "price", "developer", "genre", "genreId", "minInstalls"]
+important_things = ["title", "description", "summary", "score",
+                    "price", "developer", "genre", "genreId", "minInstalls"]
 
 c = csv.writer(open("appdata.csv", "w"))
 c.writerow(important_things)
@@ -11,7 +12,7 @@ for filename in os.listdir(os.fsencode("./appdata/")):
     try:
         j = json.loads(open("./appdata/"+filename.decode(), "r").read())
         c.writerow([
-           j["title"],
+            j["title"],
             j["description"],
             j["summary"],
             j["score"],
@@ -20,6 +21,6 @@ for filename in os.listdir(os.fsencode("./appdata/")):
             j["genre"],
             j["genreId"],
             j["minInstalls"]
-       ])
+        ])
     except:
         print(filename.decode() + " failed")
